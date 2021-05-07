11c3164f4b103663153e866b83607a3206c20a80
Yu Sato <ym.74.mn@gmail.com>
2021-05-07 19:54:58 +0900

    finish omikuji

---
+++ omikuji/main.js
@@ -0,0 +1,18 @@
+'user strict';
+
+{
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click', () => {
+    const results = ['大吉','中吉','凶','末吉'];
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = '大吉'; //5%
+    } else if (n < 0.2) {
+      btn.textContent = '中吉'; //15%
+    } else {
+      btn.textContent = '凶'; //80%
+    }
+  });
+}

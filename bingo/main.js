fc2624f4b8c3acabec905a26904ccfb35143d290
Yu Sato <ym.74.mn@gmail.com>
2021-05-11 17:03:54 +0900

    [Finish]bingo

--- bingo/main.js
+++ bingo/main.js
@@ -1,7 +1,43 @@
 'use strict';
 
 {
-  const source = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
-  // Math.floor(Math.random() * (14+1))
+  function createColumn(col) {
+    const source = [];
+    for (let i = 0; i < 15; i++) {
+      source[i] = i + 1 + 15 * col;
+    }
+    // Math.floor(Math.random() * (14+1))
+    // Math.floor(Math.random() * source.length)
+    const column = [];
+    for (let i = 0; i < 5; i ++) {
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+    }
+    return column;
+  }
+
+  function createColumns(){
+    const columns = [];
+    for (let i = 0; i < 5; i++) {
+      columns[i] =createColumn(i);
+    }
+    columns[2][2] ='FREE';
+    return columns;
+  }
+
+  function renderBingo(columns) {
+    for(let row = 0; row < 5; row++) {
+      const tr = document.createElement('tr');
+      for (let col = 0; col < 5; col++) {
+        const td = document.createElement('td');
+        td.textContent = columns[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector('tbody').appendChild(tr);
+    }
+  }
+
+  const columns = createColumns();
+  renderBingo(columns);
+
 }

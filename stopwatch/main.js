aa009b271b735d712b831a629fa365628f101b35
Yu Sato <ym.74.mn@gmail.com>
2021-05-18 15:22:30 +0900

    [Add]stopwatch

---
+++ stopwatch/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+{
+  const timer = document.getElementById('timer');
+  const start = document.getElementById('start');
+  const stop = document.getElementById('stop');
+  const reset = document.getElementById('reset');
+
+  let startTime;
+
+  function countUp(){
+    const d = new Date(Date.now() - startTime);
+    const m = String(d.getMinutes()).padStart(2,'0');
+    const s = String(d.getSeconds()).padStart(2,'0');
+    const ms = String(d.getMilliseconds()).padStart(3,'0');
+    timer.textContent = `${m}:${s}.${ms}`;
+
+    setTimeout(() => {
+      countUp();
+    }, 10);
+  }
+
+  start.addEventListener('click', () => {
+    startTime = Date.now();
+    countUp();
+  });
+}

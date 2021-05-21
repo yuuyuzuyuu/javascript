185e889586142a0ce9642815f58e885791e1742d
Yu Sato <ym.74.mn@gmail.com>
2021-05-21 18:28:37 +0900

    [Finish]quiz

--- Quiz/main.js
+++ Quiz/main.js
@@ -4,6 +4,8 @@
   const question = document.getElementById('question');
   const choices = document.getElementById('choices');
   const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
 
   const quizSet = [
     {q: 'What is A?', c: ['A1', 'A2', 'A3']},
@@ -11,8 +13,8 @@
     {q: 'What is C?', c: ['C1', 'C2', 'C3']},
   ];
   let currentNum = 0;
-
-
+  let isAnswered;
+  let score = 0;
 
   function shuffle(arr) {
     for (let i = arr.length - 1; i > 0; i--){
@@ -23,15 +25,29 @@
   }
 
   function checkAnswer(li) {
-    if (li.textContent === quizSet[currentNum].c[0]) {
-      console.log('correct');
-    } else {
-      console.log('wrong');
+    if (isAnswered){
+      return;
     }
+    isAnswered = true;
+
+    if (li.textContent === quizSet[currentNum].c[0]) {
+      li.classList.add('correct')
+      score++;
+    } else {
+      li.classList.add('wrong')
+    }
+
+    btn.classList.remove('disabled');
   }
 
   function setQuiz() {
+    isAnswered = false;
+
     question.textContent = quizSet[currentNum].q;
+
+    while(choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
 
     const shuffledChoices = shuffle([...quizSet[currentNum].c]);
     shuffledChoices.forEach(choice => { // まず最初の要素としてchoiceには'A0'が入る
@@ -42,7 +58,26 @@
       });
       choices.appendChild(li); // 親要素であるulに生成したli要素と追加することで画面に表示される
   });
+
+  if (currentNum === quizSet.length - 1) {
+    btn.textContent = 'Show Score';
+  }
   }
 
   setQuiz();
+
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('disabled')) {
+      return;
+    }
+    btn.classList.add('disabled');
+
+    if (currentNum === quizSet.length - 1) {
+      scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+      result.classList.remove('hidden');
+    } else {
+      currentNum++;
+      setQuiz();
+    }
+  });
 }

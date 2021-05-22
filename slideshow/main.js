6d596f9115625eedc81654356830336e0202c9a2
Yu Sato <ym.74.mn@gmail.com>
2021-05-22 11:07:49 +0900

    [Add]slideshow

---
+++ slideshow/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+{
+  const images = [
+    'img/pic00.png',
+    'img/pic01.png',
+    'img/pic02.png',
+    'img/pic03.png',
+    'img/pic04.png',
+    'img/pic05.png',
+    'img/pic06.png',
+    'img/pic07.png',
+  ];
+  let currentIndex = 0;
+
+  const mainImage = document.getElementById('main');
+  mainImage.src = images[currentIndex];
+
+  images.forEach((image, index) => {
+    const img = document.createElement('img');
+    img.src = image;
+
+    const li = document.createElement('li');
+    if (index === currentIndex) {
+      li.classList.add('current');
+    }
+    li.addEventListener('click', )
+
+    li.appendChild(img);
+    document.querySelector('.thumbnails').appendChild(li);
+  });
+}

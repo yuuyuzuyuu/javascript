82942edb1504570a90d8ae86fc2948ca8a815061
Yu Sato <ym.74.mn@gmail.com>
2021-05-29 22:55:39 +0900

    [Fix]omikuji2

--- omikuji2/main.js
+++ omikuji2/main.js
@@ -4,7 +4,7 @@
   const btn = document.getElementById('btn');
 
   btn.addEventListener('click', () => {
-    const results = ['lucky!', 'unhappy', 'brilliant!!', 'normal'];
+    const results = ['lucky!', 'unhappy', 'brilliant!!', 'normal', 'beautiful!'];
     const n = Math.floor(Math.random() * results.length);
     btn.textContent = results[n];
   });
